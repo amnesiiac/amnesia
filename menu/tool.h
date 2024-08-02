a241ef28c8eacee81eb44d205169b9bde49f807b
metung <metung@spine.novalocal>
2024-08-02 17:07:19 +0800

    update tool vncserver

--- menu/tool.h
+++ menu/tool.h
@@ -11,7 +11,15 @@ MenuItem* Method::get_tool_ptr(){
             {"asciinema +", {
                 {"record cast file startup (use ctrl+d to stop#asciinema rec ${record.cast}#"},
                 {"play cast file (use ctrl+c to stop)#asciinema play ${record.cast}#"},
-                {"generate a new faster played record using the existed record (asciinema play -h)#asciinema rec -c 'asciinema play -s 4 ${current.cast}' ${faster.cast}#"}}}},
+                {"generate a new faster played record using the existed record (asciinema play -h)#asciinema rec -c 'asciinema play -s 4 ${current.cast}' ${faster.cast}#"}}},
+            {"vnc +", {{"vnc config path#ls -l ~/.vnc/#"},
+                       {"vncpasswd +", {{"show manual#man vncpasswd#"},
+                                        {"interatively setup login passwd for vnc#vncpasswd#"}}},
+                       {"vncserver +", {{"show manual#man vncserver#"},
+                                        {"startup vnc server by a given id#vncserver :${uuid}#"},
+                                        {"list all vnc server live#vncserver -list#"},
+                                        {"kill a certain vnc server#vncserver -kill :${uuid}#"},
+                                        {"*config the xstartup script & behavior#cd ~/.vnc/ && vim xstartup#vnc"}}}}}},
         },
     };
     return tool_ptr;
