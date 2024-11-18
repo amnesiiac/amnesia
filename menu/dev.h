f0f9d24d405613ac900dba14f9acc0ef44916973
metung <metung@spine.novalocal>
2024-11-18 17:35:31 +0800

    update menu/dev dmesg

--- menu/dev.h
+++ menu/dev.h
@@ -25,7 +25,11 @@ MenuItem* Method::get_dev_ptr(){
             {"memory +", {{"show memory info by reading dmi tables#dmidecode -t memory#"}}},
             {"system +", {{"show system info by reading dmi tables#dmidecode -t system#"}}},
             {"bios +", {{"show bios info by reading dmi tables#dmidecode -t bios#"}}},
-            {"processor +", {{"show processor info by reading dmi tables#dmidecode -t processor#"}}}}
+            {"processor +", {{"show processor info by reading dmi tables#dmidecode -t processor#"}}},
+            {"kernel ring buffer +", {{"dmesg +", {{"show manual (print / control kernel ring buffer)#man dmesg#"},
+                                                   {"show help page#dmesg --help#"},
+                                                   {"show kernel ring buffer log in certain log level (debug, info, notice...)#dmesg -l ${log level}#"},
+                                                   {"check kvm related msg (sometime lsmod show kvm ready, but kvm still not available)#dmesg | grep -i kvm#"}}}}}},
         },
     };
     return dev_ptr;
