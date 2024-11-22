b3456eeac6b77ef52ffc9aebde070610e32936d3
metung <metung@spine.novalocal>
2024-11-22 15:19:04 +0800

    move dmesg from dev to kernel

--- menu/kernel.h
+++ menu/kernel.h
@@ -8,8 +8,12 @@ MenuItem* Method::get_kernel_ptr(){
     kernel_ptr->name = "kernel";
     kernel_ptr->submenu = {
         {"kernel +", {
-            {"grep config option inside zipped kernel config (if kernel is configured to have config.gz)#zcat /proc/config.gz | grep -i ${pattern_to_search}#"}},
-        },
+            {"grep config option inside zipped kernel config (if kernel is configured to have config.gz)#zcat /proc/config.gz | grep -i ${pattern_to_search}#"},
+            {"ring buffer +", {{"dmesg +", {{"show manual (print / control kernel ring buffer)#man dmesg#"},
+                                            {"show help page#dmesg --help#"},
+                                            {"show kernel ring buffer log in certain log level (debug, info, notice...)#dmesg -l ${log level}#"},
+                                            {"check kvm related msg (sometime lsmod show kvm ready, but kvm still not available)#dmesg | grep -i kvm#"}}}}},
+        }},
     };
     return kernel_ptr;
 }
