3d33ea2ad68e41adb25bc02d34dd990b327c54b4
metung <metung@spine.novalocal>
2024-12-23 15:47:25 +0800

    add desc hg & update menu revsion & vim

--- menu/vim.h
+++ menu/vim.h
@@ -11,11 +11,18 @@ MenuItem* Method::get_vim_ptr(){
             {"nvim img +", {{"edit nvim dockerfile#nvim /Users/mac/nvim/Dockerfile#"},
                             {"build nvim image##"},
                             {"invoke nvim image by script##"}}},
-            {"vim cmd +", {{"create & edit a new copy of current file: new.log#:sav new.log#"},
-                           {"create a new file using a range of lines#:{linestart},${lineend}w ${out.file}#"}}},
-            {"vimdiff +", {{"specify the tool as vimdiff#--tool=vimdiff#"},
-                           {"vimdiff window layout#[local, base, remote]#"},
-                           {"vimdiff switch between windows#ctrl+w,h (move to left), ctrl+w,l (move to right)#"}}},
+            {"vim buildin cmd +", {{"create & edit a new copy of current file: new.log#:sav new.log#"},
+                                   {"create a new file using a range of lines#:{linestart},${lineend}w ${out.file}#"}}},
+            {"vimdiff tool+", {{"specify the tool as vimdiff#--tool=vimdiff#"},
+                               {"vimdiff window layout#[local, base, remote]\n\
+local: local commit where you are standing on\n\
+base: the common ancestor of two split changset branches (contains older state)\n\
+remote: remote branch state which you are trying merge your local commit into (contains newer state)#"},
+                               {"vimdiff switch between windows#ctrl+w,h (move to left), ctrl+w,l (move to right)#"},
+                               {"goto next hunk#]c#"},
+                               {"goto prev hunk#[c#"},
+                               {"mod cur win by the hunk from specified win#:diffget ${window_name}#"},
+                               {"apply hunk from cur win to specified win#:diffput ${window_name}#"}}},
             {"regex match +", {{"search instance match pattern (start with lib, arbitary char in middle, end with tar.gz)#:g/lib.*tar.gz#"},
                                {"search instance match pattern in line range#/\%>9l\%<33llib.*tar.gz#"},
                                {"search instance match pattern in line range (press no to navi)#:10,32s/lib.*tar.gz//gc#"},
