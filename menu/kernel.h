49aaabf75113ca79cccc012895933e89b639f3a1
metung <metung@spine.novalocal>
2025-02-12 16:12:24 +0800

    update menu/code/debug/pdb & menu/kernel/procfs

--- menu/kernel.h
+++ menu/kernel.h
@@ -13,7 +13,7 @@ MenuItem* Method::get_kernel_ptr(){
                                             {"show help page#dmesg --help#"},
                                             {"show kernel ring buffer log in certain log level (debug, info, notice...)#dmesg -l ${log level}#"},
                                             {"check kvm related msg (sometime lsmod show kvm ready, but kvm still not available)#dmesg | grep -i kvm#"}}}}},
-            {"procfs +", {{"min mem reserved for kernel critical allocations#cat /proc/sys/vm/min_free_kbytes#"}}},
+            {"procfs +", {{"min mem reserved for kernel critical allocation (add up all memzone watermarks)#cat /proc/sys/vm/min_free_kbytes#"}}},
         }},
     };
     return kernel_ptr;
